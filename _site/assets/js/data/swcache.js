aedd6e8f8c60dd82b7ee245e564dfad781dbefef
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2023-10-18 17:49:01 -0300

    first

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,129 @@
+const resource = [
+  /* --- CSS --- */
+  '/juanmmaidana.github.io/assets/css/jekyll-theme-chirpy.css',
+
+  /* --- PWA --- */
+  '/juanmmaidana.github.io/app.js',
+  '/juanmmaidana.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/juanmmaidana.github.io/index.html',
+  '/juanmmaidana.github.io/404.html',
+
+  
+    '/juanmmaidana.github.io/categories/',
+  
+    '/juanmmaidana.github.io/tags/',
+  
+    '/juanmmaidana.github.io/archives/',
+  
+    '/juanmmaidana.github.io/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/describe.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/egestitanic.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/final.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/genre.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/headtitanic1.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/missingvalues.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/models.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/models2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/nulltable.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/pclass2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/preview.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/sobrevive.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/sobrevive0.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/test.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/test2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/test3.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/test4.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/test5.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/tiaticHead1.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/tiaticprewview2.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/titanicpreview.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/total.png',
+    '/juanmmaidana.github.io/assets/img/favicons/Titanic/total2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/android-chrome-192x192.png',
+    '/juanmmaidana.github.io/assets/img/favicons/android-chrome-512x512.png',
+    '/juanmmaidana.github.io/assets/img/favicons/apple-touch-icon.png',
+    '/juanmmaidana.github.io/assets/img/favicons/favicon-16x16.png',
+    '/juanmmaidana.github.io/assets/img/favicons/favicon-32x32.png',
+    '/juanmmaidana.github.io/assets/img/favicons/favicon.ico',
+    '/juanmmaidana.github.io/assets/img/favicons/mlgod.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/mstile-150x150.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/LRegg.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/LRegg2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/drop.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/dtree.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/dtree2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/filters.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/head.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/head2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/info.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/knn.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/knn2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/label.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/label3.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/matrix.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/naivebayes.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/naivebayesConfusion.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/normal.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/obus2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/preview.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/redshift.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/redshift2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/rforest.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/rforest2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/sdss.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/shape.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/shapeFinal.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/shapeout.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/smote.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/smotedesp.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/xg.png',
+    '/juanmmaidana.github.io/assets/img/favicons/stellar/xg2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/boxplot.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/description.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/fla.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/head.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/histograms.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/histograms2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/info.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/knn.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/matrix.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/out2.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/randomf.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/svc.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/totaldesc.png',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/wine2.jpg',
+    '/juanmmaidana.github.io/assets/img/favicons/yo2.jpg',
+    '/juanmmaidana.github.io/assets/js/dist/categories.min.js',
+    '/juanmmaidana.github.io/assets/js/dist/commons.min.js',
+    '/juanmmaidana.github.io/assets/js/dist/home.min.js',
+    '/juanmmaidana.github.io/assets/js/dist/misc.min.js',
+    '/juanmmaidana.github.io/assets/js/dist/page.min.js',
+    '/juanmmaidana.github.io/assets/js/dist/post.min.js',
+    '/juanmmaidana.github.io/assets/img/favicons/.DS_Store',
+    '/juanmmaidana.github.io/assets/img/favicons/wine/.DS_Store'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'localhost:4000',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [];
+
