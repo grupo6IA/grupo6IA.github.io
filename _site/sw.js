143cbe514b468e97e320ec235d1827444b384384
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2023-10-18 18:22:18 -0300

    second

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/juanmmaidana.github.io/assets/js/data/swcache.js');
+self.importScripts('/grupo6IA.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231015.194308';
+const cacheName = 'chirpy-20231018.180729';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
